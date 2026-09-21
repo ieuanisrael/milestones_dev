6907711e278bab2ce16f7c00190298491e1b3ff3
CricketNSW <96504601+CricketNSW@users.noreply.github.com>
2026-09-21 12:15:00 +1000

    [ludis] Update file R/modules/mod_conditional_filters.R

--- R/modules/mod_conditional_filters.R
+++ R/modules/mod_conditional_filters.R
@@ -84,7 +84,7 @@ conditionalFiltersUI <- function(id) {
   )
 }
 
-conditionalFiltersServer <- function(id, con, internal_con) {
+conditionalFiltersServer <- function(id, con) {
   moduleServer(id, function(input, output, session) {
 
     output$filter_legend <- renderUI({
