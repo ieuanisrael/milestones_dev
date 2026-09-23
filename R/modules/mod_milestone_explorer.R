caefea5cd32c38d1238bd9e924b517a275cc02b1
Ieuan Israel <ieuan.israel@cricketnsw.com.au>
2026-09-23 16:25:44 +1000

    change color of highlighted player rows

--- R/modules/mod_milestone_explorer.R
+++ R/modules/mod_milestone_explorer.R
@@ -60,7 +60,7 @@ milestoneExplorerServer <- function(id, con, internal_con) {
           ),
           
           tags$div(
-            style = "background-color:#fff2cc; padding: 12px 16px; border-radius: 6px; box-shadow: 0 2px 4px rgba(0,0,0,0.05);",
+            style = "background-color: #00b0f0; padding: 12px 16px; border-radius: 6px; box-shadow: 0 2px 4px rgba(0,0,0,0.05);",
             
             # Message body
             tags$div(
@@ -126,7 +126,7 @@ milestoneExplorerServer <- function(id, con, internal_con) {
       {
         req(filters()$series)
 
-        team_list <- data.frame(ams_id = '')
+        team_list <- data.frame(ams_id = '2070026')
         
         datatable(
           leaderboard_data(),
@@ -142,7 +142,7 @@ milestoneExplorerServer <- function(id, con, internal_con) {
             target = 'row',
             backgroundColor = styleEqual(
               team_list$ams_id, # The exact names to look for
-              rep('#fff2cc', length(team_list$ams_id)),  # Highlights matches in light yellow
+              rep('#00b0f0', length(team_list$ams_id)),  # Highlights matches in light yellow
               default = '#ffffff'
             )
           )
