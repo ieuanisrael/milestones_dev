4c42d37b932866304bc4a7b980a696ce987af7f7
Ieuan Israel <ieuan.israel@cricketnsw.com.au>
2026-09-26 08:50:16 +1000

    comment out for ludis

--- email/milestone_snapshots.R
+++ email/milestone_snapshots.R
@@ -2,8 +2,8 @@ library(glue)
 library(lubridate)
 library(dplyr)
 
-Sys.setenv("MILESTONES_USE_SAMPLE" = 0) # uncomment for database
-Sys.setenv("MILESTONES_USE_LUDIS" = 1)
+# Sys.setenv("MILESTONES_USE_SAMPLE" = 0) # uncomment for database
+# Sys.setenv("MILESTONES_USE_LUDIS" = 0)
 
 if (!file.exists("app.R")) {
   stop("Run this script from the milestones_dev project root.")
